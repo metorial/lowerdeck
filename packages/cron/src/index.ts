c13951a741b8c99b5d4a5b5c5c1376c6d2eefdab
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-12-13 16:35:40 -0800

    Add queue packages

---
+++ packages/cron/src/index.ts
@@ -0,0 +1,63 @@
+import { IQueueProcessor } from '@lowerdeck/queue';
+import { parseRedisUrl } from '@lowerdeck/redis';
+import { Queue, Worker } from 'bullmq';
+
+let log = (...any: any[]) => console.log('[CRON MANAGER]:', ...any);
+
+let seenNames = new Set<string>();
+
+export let createCron = (
+  opts: {
+    name: string;
+    cron: string;
+    redisUrl: string;
+  },
+  handler: () => Promise<void>
+): IQueueProcessor => {
+  if (seenNames.has(opts.name)) {
+    throw new Error(`Cron with name ${opts.name} already exists`);
+  }
+
+  let connection = parseRedisUrl(opts.redisUrl);
+  let queue = new Queue(opts.name, {
+    connection,
+    defaultJobOptions: {
+      removeOnComplete: true,
+      removeOnFail: true
+    }
+  });
+
+  return {
+    start: async () => {
+      log(`Starting cron job ${opts.name} to run every ${opts.cron} using bullmq`);
+
+      await queue.upsertJobScheduler(
+        'cron',
+        {
+          pattern: opts.cron
+        },
+        {
+          opts: {
+            removeDependencyOnFailure: true,
+            removeOnComplete: true,
+            removeOnFail: true,
+            keepLogs: 0
+          }
+        }
+      );
+
+      let worker = new Worker(
+        opts.name,
+        async () => {
+          log(`Running cron job ${opts.name}`);
+          await handler();
+        },
+        { connection }
+      );
+
+      return {
+        close: worker.close.bind(worker)
+      };
+    }
+  };
+};
